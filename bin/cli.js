070e78d5fe7d9bec7526f339d2e4df58cd354240
李云飞 <liyf.2@jifenn.com>
2021-06-30 16:26:10 +0800

    feat: 🎸 third step
    add commander to parse args

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
+const program = require('commander')
 
 console.log("i'm a cli")
+
+// 打印参数
+console.log(process.argv)
+
+program
+  .command('create <projectName>')
+  .description('create a new project')
+  .alias('c')
+  .option('-r, --react', 'react template')
+  .option('-v, --vue', 'vue template')
+  .option('-v2, --vue2', 'vue2 template')
+  .option('-v3, --vue3', 'vue3 template')
+  .action((projectName, options) => {
+    console.log(projectName, options)
+  })
+program.version('1.0.0').parse(process.argv)
